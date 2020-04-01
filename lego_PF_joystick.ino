41f018d48bc9c98fd96d81ecab586005e28c74d0
maurodani76 <44203381+maurodani76@users.noreply.github.com>
2020-04-01 18:37:23 +0200

    Versione definitiva per torretta gru

--- lego_PF_joystick.ino
+++ lego_PF_joystick.ino
@@ -21,32 +21,32 @@ void setup()
 void loop()
 {
   val1 = val;
-  val = map(analogRead(A1), -20,1023, -7,7); // aggiustare i valori estremi per trimmare lo zero centrale
+  val = map(analogRead(A7), -20,1003, -7,7); // aggiustare i valori estremi per trimmare lo zero centrale; A7 è l'ingresso scelto nel progetto torretta gru
   if ( val != val1 && val >= 0 ) {
     r = 0;                                   // r viene resettato a 0 se il potenziometro viene ruotato troppo velocemente
     f = val;
       if ( f == 0 ) {
         lego.SingleOutput(0, PWM_FLT, RED, CH1);
         }
-      if ( f == 1 ) {
+      /*if ( f == 1 ) {                 // è commentato per aumentare di molto la zona morta dello 0 centrale, la velocità 1 sulla rotazione torretta gru è inutile
         lego.SingleOutput(0, PWM_FWD1, RED, CH1);
-        }
-      if ( f == 2 ) {
+        }*/
+      else if ( f == 2 ) {
         lego.SingleOutput(0, PWM_FWD2, RED, CH1);
         }
-      if ( f == 3 ) {
+      else if ( f == 3 ) {
         lego.SingleOutput(0, PWM_FWD3, RED, CH1);
         }
-      if ( f == 4 ) {
+      else if ( f == 4 ) {
         lego.SingleOutput(0, PWM_FWD4, RED, CH1);
         }
-      if ( f == 5 ) {
+      else if ( f == 5 ) {
         lego.SingleOutput(0, PWM_FWD5, RED, CH1);
         }
-      if ( f == 6 ) {
+      else if ( f == 6 ) {
         lego.SingleOutput(0, PWM_FWD6, RED, CH1);
         }
-      if ( f == 7 ) {
+      else if ( f == 7 ) {
         lego.SingleOutput(0, PWM_FWD7, RED, CH1);
 }
     delay(100);
@@ -57,25 +57,25 @@ void loop()
       if ( r == 0 ) {
         lego.SingleOutput(0, PWM_FLT, RED, CH1);
         }
-      if ( r == 1 ) {
+      /*if ( r == 1 ) {                 // è commentato per aumentare di molto la zona morta dello 0 centrale, la velocità 1 sulla rotazione torretta gru è inutile
         lego.SingleOutput(0, PWM_REV1, RED, CH1);
-        }
-      if ( r == 2 ) {
+        }*/
+      else if ( r == 2 ) {
         lego.SingleOutput(0, PWM_REV2, RED, CH1);
         }
-      if ( r == 3 ) {
+      else if ( r == 3 ) {
         lego.SingleOutput(0, PWM_REV3, RED, CH1);
         }
-      if ( r == 4 ) {
+      else if ( r == 4 ) {
         lego.SingleOutput(0, PWM_REV4, RED, CH1);
         }
-      if ( r == 5 ) {
+      else if ( r == 5 ) {
         lego.SingleOutput(0, PWM_REV5, RED, CH1);
         }
-      if ( r == 6 ) {
+      else if ( r == 6 ) {
         lego.SingleOutput(0, PWM_REV6, RED, CH1);
         }
-      if ( r == 7 ) {
+      else if ( r == 7 ) {
         lego.SingleOutput(0, PWM_REV7, RED, CH1);
   }
     delay(100);
