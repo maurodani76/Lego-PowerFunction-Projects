1a9875da6cf76f2a3c68182f8183939d10d68a88
maurodani76 <44203381+maurodani76@users.noreply.github.com>
2020-12-29 23:26:43 +0100

    V2.0 - versione funzionante
    codice più snello, da migliorare ulteriormente qualche dettaglio

---
+++ lego_PF_pulsanti_test.ino
@@ -0,0 +1,49 @@
+/*
+Codice base per aumentare e diminuire il valore di PWM con due pulsanti
+Il pulsante centrale ferma tutto, riportando a 0 i valori delle due variabili
+La seriale è solo per test dei valori riportati.
+*/
+#include <legopowerfunctions.h>
+
+#define IRLed 11      // pin al quale è collegato il led IR (usare transistor per pilotare il led IR!)
+#define LEFT 7        // pin pulsante LEFT
+#define STOP 8        // pin pulsante STOP
+#define RIGHT 9       // pin pulsante RIGHT
+
+
+LEGOPowerFunctions lego(11);
+int speed = 0;
+int prevspeed = 0;
+  
+void setup()
+{
+  // Serial.begin(9600);
+  pinMode(RIGHT, INPUT);
+  pinMode(STOP, INPUT);
+  pinMode(LEFT, INPUT);
+ 
+}
+
+void loop()
+{
+  
+    if ( digitalRead(RIGHT) == HIGH && (speed < 7 || speed > 8) && speed < 16 ) {
+        speed++;  
+          if ( speed == 16 ) {
+          speed = 0;
+          }
+        lego.SingleOutput(0, speed , RED, CH1);  
+        delay(150);
+    }
+    else if ( digitalRead(LEFT) == HIGH && (speed < 8 || speed > 9) && speed > 0 ) {
+        speed--;
+        lego.SingleOutput(0, speed , RED, CH1);
+        delay(150);
+    }      
+    else if ( digitalRead(LEFT) == HIGH && speed == 0 ) {
+        speed = 16;
+        speed--;
+        lego.SingleOutput(0, speed , RED, CH1);
+        delay(150);
+    }
+}
