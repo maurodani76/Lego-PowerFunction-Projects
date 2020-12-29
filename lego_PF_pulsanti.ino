51cdb62470f1a12e7244b9c9b5b2465677de1208
maurodani76 <44203381+maurodani76@users.noreply.github.com>
2020-12-29 16:06:06 +0100

    Update lego_PF_pulsanti.ino

--- lego_PF_pulsanti.ino
+++ lego_PF_pulsanti.ino
@@ -33,6 +33,108 @@ void loop()
       if ( f == 1 ) {
         lego.SingleOutput(0, PWM_FWD1, BLUE, CH4);
         }
+      if ( f == 2 ) {
+        lego.SingleOutput(0, PWM_FWD2, BLUE, CH4);
+        }
+      if ( f == 3 ) {
+        lego.SingleOutput(0, PWM_FWD3, BLUE, CH4);
+        }
+      if ( f == 4 ) {
+        lego.SingleOutput(0, PWM_FWD4, BLUE, CH4);
+        }
+      if ( f == 5 ) {
+        lego.SingleOutput(0, PWM_FWD5, BLUE, CH4);
+        }
+      if ( f == 6 ) {
+        lego.SingleOutput(0, PWM_FWD6, BLUE, CH4);
+        }
+      if ( f == 7 ) {
+        lego.SingleOutput(0, PWM_FWD7, BLUE, CH4);
+        }
+    } else {
+        r = r-1;
+      if ( r == 1 ) {
+        lego.SingleOutput(0, PWM_REV1, BLUE, CH4);
+        }
+      if ( r == 2 ) {
+        lego.SingleOutput(0, PWM_REV2, BLUE, CH4);
+        }
+      if ( r == 3 ) {
+        lego.SingleOutput(0, PWM_REV3, BLUE, CH4);
+        }
+      if ( r == 4 ) {
+        lego.SingleOutput(0, PWM_REV4, BLUE, CH4);
+        }
+      if ( r == 5 ) {
+        lego.SingleOutput(0, PWM_REV5, BLUE, CH4);
+        }
+      if ( r == 6 ) {
+        lego.SingleOutput(0, PWM_REV6, BLUE, CH4);
+        }
+      if ( r == 7 ) {
+        lego.SingleOutput(0, PWM_REV7, BLUE, CH4);
+        }
     }
+    delay(100);
   }
+  if ( digitalRead(LEFT) == HIGH && r < 7) {
+    if ( f == 0 ) {
+      r = r+1;
+      if ( r == 1 ) {
+        lego.SingleOutput(0, PWM_REV1, BLUE, CH4);
+        }
+      if ( r == 2 ) {
+        lego.SingleOutput(0, PWM_REV2, BLUE, CH4);
+        }
+      if ( r == 3 ) {
+        lego.SingleOutput(0, PWM_REV3, BLUE, CH4);
+        }
+      if ( r == 4 ) {
+        lego.SingleOutput(0, PWM_REV4, BLUE, CH4);
+        }
+      if ( r == 5 ) {
+        lego.SingleOutput(0, PWM_REV5, BLUE, CH4);
+        }
+      if ( r == 6 ) {
+        lego.SingleOutput(0, PWM_REV6, BLUE, CH4);
+        }
+      if ( r == 7 ) {
+        lego.SingleOutput(0, PWM_REV7, BLUE, CH4);
+        }
+    } else {
+        f = f-1;
+        if ( f == 1 ) {
+        lego.SingleOutput(0, PWM_FWD1, BLUE, CH4);
+        }
+      if ( f == 2 ) {
+        lego.SingleOutput(0, PWM_FWD2, BLUE, CH4);
+        }
+      if ( f == 3 ) {
+        lego.SingleOutput(0, PWM_FWD3, BLUE, CH4);
+        }
+      if ( f == 4 ) {
+        lego.SingleOutput(0, PWM_FWD4, BLUE, CH4);
+        }
+      if ( f == 5 ) {
+        lego.SingleOutput(0, PWM_FWD5, BLUE, CH4);
+        }
+      if ( f == 6 ) {
+        lego.SingleOutput(0, PWM_FWD6, BLUE, CH4);
+        }
+      if ( f == 7 ) {
+        lego.SingleOutput(0, PWM_FWD7, BLUE, CH4);
+        }
+    }
+    delay(100);
+  }
+  if ( digitalRead(STOP) == HIGH ) {
+    f = 0;
+    r = 0;
+    lego.SingleOutput(0, PWM_FLT, BLUE, CH4);
+  }
+  // Serial.print(f);
+  // Serial.print(",");
+  // Serial.println(r);
+  delay(100);
+  
 }
